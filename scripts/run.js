e3fe5c0a3c9564c9e8eafab1addc6e4828ed2cd2
Linus Weigand <linusweigand@protonmail.com>
2022-04-08 13:34:02 +0200

    added contract functions to run script

--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,20 @@
 const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
+
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
+
     console.log("Contract deployed to:", waveContract.address);
+    console.log("Contract deployed by:", owner.address);
+
+    let waveCount;
+    waveCount = await waveContract.getTotalWaveCount();
+
+    let waveTxn = await waveContract.wave();
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaveCount();
 };
 
 const runMain = async () => {
