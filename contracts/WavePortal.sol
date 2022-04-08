820c773997065352da99eace649725d1b471a152
Linus Weigand <linusweigand@protonmail.com>
2022-04-08 14:01:37 +0200

    added msg.sender array

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,18 +6,25 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
+    address[] wavers;
     
     constructor() {
         console.log("Yoo I'm a smart contract");
+
     }
 
     function wave() public {
-        totalWaves += 1;
+        totalWaves++;
+        wavers.push(msg.sender);
         console.log("%s has waved!", msg.sender);
     }
 
     function getTotalWaves() public view returns (uint256) {
         console.log("We have %d total waves!", totalWaves);
         return totalWaves;
+    }
+
+    function getTotalWavers() public  view returns (address[] memory) {
+        return wavers;
     }
 }
